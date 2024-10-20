41267864c26e171952fad5c110436540183af346
Shubham-git7 <166055114+Shubham-git7@users.noreply.github.com>
2024-10-20 02:44:27 -0400

    Update AdminProjects.js

--- src/pages/Admin/AdminProjects.js
+++ src/pages/Admin/AdminProjects.js
@@ -24,12 +24,12 @@ const AdminProjects = () => {
 
       // Send the experience directly instead of wrapping it in an array
       if (selectedItemForEdit) {
-        response = await axios.post("/api/portfolio/update-project", {
+        response = await axios.post("https://server-portfolio-sfdo.onrender.com/api/portfolio/update-project", {
           ...values,
           _id: selectedItemForEdit._id,
         });
       } else {
-        response = await axios.post("/api/portfolio/add-project", values);
+        response = await axios.post("https://server-portfolio-sfdo.onrender.com/api/portfolio/add-project", values);
       }
 
       dispatch(hideLoading());
@@ -50,7 +50,7 @@ const AdminProjects = () => {
   const onDelete = async (item) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post("/api/portfolio/delete-project", {
+      const response = await axios.post("https://server-portfolio-sfdo.onrender.com/api/portfolio/delete-project", {
         _id: item._id,
       });
       dispatch(hideLoading());
